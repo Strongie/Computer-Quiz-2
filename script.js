3525f1dd214b659cf48813dcf98c06acb621ac34
Strongie <nickstrong665@gmail.com>
2022-09-24 14:38:14 +1000

    added more global variables from html

---
+++ script.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["1. Strings","2. Booleans", "3. Alerts", "4. Numbers"],
+        answer: "2. Booleans"
+    },
+
+    {
+        question: "The condition in an if / else statement is enclosed within ....",
+        options: ["1. Quotes", "2. Curly Brackets", "3. Paranthesis", "4. Square Brackets"],
+        answer: "3. Paranthesis"
+    },
+
+    {
+        question: "Arrays in Javascript can be used to store:",
+        options: ['1. Numbers', '2. Booleans', '3. Strings', '4. All of the Above'],
+        answer: "4. All of the Above"
+    },
+
+    {
+        question: "String values must be enclosed within ....... when being assigned variables.",
+        options: ["1. Commas","2. Curly Brackets", "3. Quotes","4. Paranthesis"],
+        answer: "2. Curly Brackets"
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["1. Javascript","2. Terminal/Bash", "3. For Loops", "4. Console.log"],
+        answer: "2. Console.log"
+    },
+
+];
+
