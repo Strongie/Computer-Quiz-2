5ae83e3d22d898f1a7953c1aa1bd072bb8f447f4
Strongie <nickstrong665@gmail.com>
2022-09-24 23:51:37 +1000

    added some event listeners and functions

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 
-// Add the questions
-var questions = [
+const questions = [
     {
         question: "Commonly used data types DO NOT include:",
-        options: ["1. Strings","2. Booleans", "3. Alerts", "4. Numbers"],
-        answer: "2. Booleans"
+        options: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+        answer: "2. Booleans",
     },
 
     {
-        question: "The condition in an if / else statement is enclosed within ....",
+        question: "The condition in an if / else statement is enclosed within ....?",
         options: ["1. Quotes", "2. Curly Brackets", "3. Paranthesis", "4. Square Brackets"],
-        answer: "3. Paranthesis"
+        answer: "3. Paranthesis",
     },
 
     {
         question: "Arrays in Javascript can be used to store:",
-        options: ['1. Numbers', '2. Booleans', '3. Strings', '4. All of the Above'],
-        answer: "4. All of the Above"
+        options: ["1. Numbers", "2. Booleans", "3. Strings", "4. All of the Above"],
+        answer: "4. All of the Above",
     },
 
     {
         question: "String values must be enclosed within ....... when being assigned variables.",
         options: ["1. Commas","2. Curly Brackets", "3. Quotes","4. Paranthesis"],
-        answer: "2. Curly Brackets"
+        answer: "2. Curly Brackets",
     },
 
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         options: ["1. Javascript","2. Terminal/Bash", "3. For Loops", "4. Console.log"],
-        answer: "2. Console.log"
+        answer: "2. Console.log",
     },
 
-];
+    ];
+
+console.log(questions);
 
 //Get ids from html and set to variables
 
@@ -44,7 +45,6 @@ var clearHighScore = document.querySelector('#clear-high-score-btn');
 var timer = document.querySelector('#timer');
 var highScores = document.querySelector('#highscores');
 var quizQuestion = document.querySelector('#quiz-question');
-var options = questions[options];
 
 // Timer
 
@@ -89,40 +89,69 @@ function showHighScore (){
 
 };
 
+//place question in quizcard
 function askQuestion(){
   currentQuestion = 0;
     var query = questions[currentQuestion];
-    var options = questions.options;
+    var options = query.options;
     
     if (currentQuestion<questions.length){
         quizQuestion.textContent = query.question;
 
     };
 
-    
-for (var i = 0; i < options.length; i++) { 
-    let choice = options[i];
-    console.log(choice);
+    console.log("object options", options);
+
+    for (var i = 0; i < options.length; i++) { 
+    var choice = options[i];
+        console.log(choice);
+    var answerbtn = document.querySelector('#answer-button1' + i);
+        answerbtn.textContent = choice;
     // currentQuestion ++;
     // quizQuestion.textContent = query.question
-}
+};
 for (var i = 0; i < questions.length; i++) { 
     console.log(questions[i]);
 
 };
 };
 
+//Check answers
+
+
+//save scores
+
+function saveScores (){
+        
+
+}
 
 
 
+//event listener for go back to start
+
+goBackButton.addEventListener("click", restart);
 
 
-// var keys = Object.keys(questions);
-// console.log(keys);
-//place question in quizcard
+// return to start
+function restart (){
+    welcomeCard.style.display = 'block';
+    highScores.style.display = 'none';
+};
+
+//clear high scores
+
+clearHighScore.addEventListener("click", clearScoreBoard);
+
+
+function clearScoreBoard (){
+    localStorage.clear();
+}; 
+
+
 
     
 
 
 
- //   quizQuestion.textContent = questions.question
+ 
